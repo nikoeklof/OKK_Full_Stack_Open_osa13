3852b8585796ee733ebe9d5ed2a5dca54832b68e
Niko / Nigey <dataaja.96@hotmail.com>
2023-01-19 14:05:43 +0200

    osa13A valmis

---
+++ cli.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+const url = "http://localhost:4001/api/blogs";
+// axios.get(url).then((res) => {
+//   res.data.forEach((blog) => {
+//     console.log(blog.author + ":", blog.title, ",", blog.url, ",", blog.likes);
+//   });
+// });
--- index.js
+++ index.js
@@ -1,16 +1,79 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize, Model, DataTypes, INTEGER } = require("sequelize");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { string } = require("yargs");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const app = express();
+app.use(express.json());
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    sequelize.close();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
+const jsonParser = bodyParser.json();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+});
+
+class Blog extends Model {}
+Blog.init(
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    author: { type: DataTypes.TEXT, allowNull: false },
+    url: { type: DataTypes.TEXT, allowNull: false },
+    title: { type: DataTypes.TEXT, allowNull: false },
+    likes: { type: DataTypes.INTEGER, defaultValue: 0 },
+  },
+  { sequelize, underscored: true, timestamps: false, modelName: "blog" }
+);
+app.get("/api/blogs", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+app.get("/api/blogs/:id", async (req, res) => {
+  const blogs = await Blog.findByPk(req.params.id);
+  res.json(blogs);
+});
+app.post("/api/blogs/", async (req, res) => {
+  console.log(isNaN(req.body.likes));
+  if (req.body) {
+    const blog = req.body;
+    if (blog.author && blog.url && blog.title) {
+      if (!blog.likes || isNaN(blog.likes)) {
+        const reformattedBlog = {
+          author: blog.author,
+          url: blog.url,
+          title: blog.title,
+          likes: 0,
+        };
+        Blog.create(reformattedBlog);
+        return res.end(`Blog \"${reformattedBlog.title}\" Added.`);
+      } else {
+        Blog.create(blog);
+        return res.end(`Blog \"${blog.title}\" Added.`);
+      }
+    }
+  } else {
+    return res.end("400 Bad Request.");
   }
-};
+});
+app.delete("/api/blogs/:id", async (req, res) => {
+  console.log(req.params);
+  if (req.params.id) {
+    Blog.destroy({ where: { id: req.params.id } }).then(
+      function (rowDeleted) {
+        if (rowDeleted === 1) {
+          res.status(200).end(`Blog Deleted.`);
+        }
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+  } else {
+    res.status(400).end("Bad request, check parameters");
+  }
+});
 
-main();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
